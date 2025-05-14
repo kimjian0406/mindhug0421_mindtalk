16299feaa24795acd762e12358de47c81a9bedc1
kimjian0406 <oheunsuhhy3@naver.com>
2025-05-14 14:11:58 +0900

    Delete mindhug directory

--- mindhug/core_comfig.py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import timedelta
-
-SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 1시간
-
--- mindhug/schemas_user.py
+++
@@ -1,8 +0,0 @@
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
-
